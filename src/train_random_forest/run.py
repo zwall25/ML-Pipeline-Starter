129766002085ad85d4426b19542f4bf6a2a70dfa
Zachary <zwall25@wgu.edu>
2025-11-26 14:43:31 -0600

    Step 4 Second half completed

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -73,7 +73,7 @@ def go(args):
 
     ######################################
     # Fit the pipeline sk_pipe by calling the .fit method on X_train and y_train
-    # YOUR CODE HERE
+    sk_pipe.fit(X_train, y_train)
     ######################################
 
     # Compute r2 and MAE
@@ -95,8 +95,12 @@ def go(args):
     ######################################
     # Save the sk_pipe pipeline as a mlflow.sklearn model in the directory "random_forest_dir"
     # HINT: use mlflow.sklearn.save_model
+    signature = mlflow.models.infer_signature(X_val, y_pred)
     mlflow.sklearn.save_model(
+        sk_model = sk_pipe,
+        path = "random_forest_dir",
         # YOUR CODE HERE
+        signature = signature,
         input_example = X_train.iloc[:5]
     )
     ######################################
@@ -119,7 +123,7 @@ def go(args):
     # Here we save variable r_squared under the "r2" key
     run.summary['r2'] = r_squared
     # Now save the variable mae under the key "mae".
-    # YOUR CODE HERE
+    run.summary['mae'] = mae
     ######################################
 
     # Upload to W&B the feture importance visualization
@@ -162,8 +166,9 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
     # 1 - A SimpleImputer(strategy="most_frequent") to impute missing values
     # 2 - A OneHotEncoder() step to encode the variable
     non_ordinal_categorical_preproc = make_pipeline(
-        # YOUR CODE HERE
-    )
+        SimpleImputer(strategy="most_frequent"),
+        OneHotEncoder()
+        )
     ######################################
 
     # Let's impute the numerical columns to make sure we can handle missing values
@@ -225,8 +230,8 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
 
     sk_pipe = Pipeline(
         steps =[
-        # YOUR CODE HERE
-        ]
+            ("preprocessor", preprocessor),        ("random_forest", random_forest)
+            ]
     )
 
     return sk_pipe, processed_features
