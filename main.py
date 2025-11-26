f479dd7e670fb6d8f9f4c0d1bd6ee7966f287c93
Zachary <zwall25@wgu.edu>
2025-11-26 15:01:40 -0600

    Step 5 completed

--- main.py
+++ main.py
@@ -117,13 +117,16 @@ def go(config: DictConfig):
             pass
 
         if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "components", "test_regression_model"),
+                "main",
+                parameters={
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest",
+                },
+            )
 
             pass
-
 
 if __name__ == "__main__":
     go()
